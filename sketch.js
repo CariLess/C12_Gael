cd1d9c68db31279b9841e9db0bea60273429506b
CariLess <90604545+CariLess@users.noreply.github.com>
2021-11-19 19:37:03 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+var trex, trex_running;
+var ground, groundimg;
+var invisbleGround;
+var clouds, cloudsImg;
+
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  groundimg = loadImage("ground2.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  //crear sprite del t-rex.
+  trex = createSprite(50, 160, 20, 50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  trex.x = 50;
+
+  ground = createSprite(200, 180, 400, 20);
+  ground.addImage("ground", groundimg);
+  edges = createEdgeSprites();
+
+  invisbleGround = createSprite(200,190,400,10);
+  invisbleGround.visible = false;
+
+  var rand = Math.round(random(10,60));
+  console.log(rand);
+
+
+}
+
+function draw() {
+  background("white")
+//console.log(trex.y);
+  if (keyDown("space") && trex.y >= 161) {
+    trex.velocityY = -10;
+  }
+  trex.velocityY = trex.velocityY + 0.5;
+
+  ground.velocityX = -2;
+
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
+
+  trex.collide(invisbleGround);
+
+  spawnClouds();
+  //0 derecha
+  //1 izquierda
+  //2 arriba
+  //3 abajo
+
+  drawSprites();
+}
+
+function spawnClouds(){
+
+}
